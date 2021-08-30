98986ffdd1b53d650066d0b706baa52fc52597d3
Daniel Calvin <arockne2003@yahoo.com>
2021-08-30 10:53:02 -0700

    Handle upVote and downVote

--- src/components/VideoDescription.js
+++ src/components/VideoDescription.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function VideoDescription({ title, views, createdAt, upVotes, downVotes}) {
+  const [upVote, setUpVote] = useState(upVotes);
+  const [downVote, setDownVote] = useState(downVotes)
+
+  function handleUpVote() {
+    setUpVote(upVote => upVote + 1)
+  }
+
+  function handleDownVote() {
+    setDownVote(downVote => downVote + 1)
+  }
+
   return (
     <aside>
       <h1>{title}</h1>
       <p>{views} Views | Uploaded {createdAt}</p>
-      <button>{upVotes} <span>👍</span></button>
-      <button>{downVotes} <span>👎</span></button>
+      <button onClick={handleUpVote}>{upVote} <span>👍</span></button>
+      <button onClick={handleDownVote}>{downVote} <span>👎</span></button>
     </aside>
   )
 }
